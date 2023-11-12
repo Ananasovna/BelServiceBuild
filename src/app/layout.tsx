02782f2eb66bc7810101f57134abd4d7b5797d26
Ananasovna <a.n.ushkova@yandex.ru>
2023-11-12 17:10:54 +0200

    feat: add favicon variants

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,16 +9,16 @@ import './globals.css'
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang={'en'}>
-      {/*<Head>*/}
-      {/*  /!*<link href={'./favicon.ico'} rel={'shortcut icon'} />*!/*/}
-      {/*  /!*<link href={'/favicon/apple-touch-icon.png'} rel={'apple-touch-icon'} sizes={'180x180'} />*!/*/}
-      {/*  /!*<link href={'/favicon/favicon-32x32.png'} rel={'icon'} sizes={'32x32'} type={'image/png'} />*!/*/}
-      {/*  /!*<link href={'/favicon/favicon-16x16.png'} rel={'icon'} sizes={'16x16'} type={'image/png'} />*!/*/}
-      {/*  /!*<link href={'/favicon/site.webmanifest'} rel={'manifest'} />*!/*/}
-      {/*  <link color={'#5bbad5'} href={'/favicon/safari-pinned-tab.svg'} rel={'mask-icon'} />*/}
-      {/*  <meta content={'#da532c'} name={'msapplication-TileColor'} />*/}
-      {/*  <meta content={'#ffffff'} name={'theme-color'} />*/}
-      {/*</Head>*/}
+      <Head>
+        <link href={'./favicon.ico'} rel={'shortcut icon'} />
+        <link href={'/favicon/apple-touch-icon.png'} rel={'apple-touch-icon'} sizes={'180x180'} />
+        <link href={'/favicon/favicon-32x32.png'} rel={'icon'} sizes={'32x32'} type={'image/png'} />
+        <link href={'/favicon/favicon-16x16.png'} rel={'icon'} sizes={'16x16'} type={'image/png'} />
+        <link href={'/favicon/site.webmanifest'} rel={'manifest'} />
+        <link color={'#5bbad5'} href={'/favicon/safari-pinned-tab.svg'} rel={'mask-icon'} />
+        <meta content={'#da532c'} name={'msapplication-TileColor'} />
+        <meta content={'#ffffff'} name={'theme-color'} />
+      </Head>
       <body className={'main'}>
         <Header />
         <div className={'content'}>{children}</div>
