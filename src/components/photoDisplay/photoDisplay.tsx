525c7cb1c9a9ee082ad1a2886d66510f3bb0844d
Ananasovna <a.n.ushkova@yandex.ru>
2023-11-22 19:36:43 +0200

    feat: add animation

--- src/components/photoDisplay/photoDisplay.tsx
+++ src/components/photoDisplay/photoDisplay.tsx
@@ -1,18 +1,31 @@
+'use client'
 import { Typography } from '@/common/components/typography/typography'
 import Image from 'next/image'
 
 import s from './photoDisplay.module.scss'
 
 import mainBg from '../../../public/image/mainBg6.jpg'
+import { clsx } from 'clsx'
+import { useState } from 'react'
 export const PhotoDisplay = () => {
+  const [animation, setAnimation] = useState<boolean>(false)
+
   return (
     <div className={s.wrapper}>
-      <div className={s.bgWrapper}>
-        <Image alt={'Фоновое изображение'} className={s.mainBg} placeholder={'blur'} src={mainBg} />
+      <div className={clsx(s.display, animation && s.animationDisplay)}>
+        <div className={s.bgWrapper}>
+          <Image
+            onLoadingComplete={() => setAnimation(true)}
+            priority={true}
+            alt={'Фоновое изображение'}
+            className={s.mainBg}
+            src={mainBg}
+          />
+        </div>
+        <Typography className={clsx(s.title, animation && s.animationTitle)} variant={'large'}>
+          Обслуживание и ремонт коммерческих помещений
+        </Typography>
       </div>
-      <Typography className={s.title} variant={'large'}>
-        Обслуживание и ремонт коммерческих помещений
-      </Typography>
     </div>
   )
 }
